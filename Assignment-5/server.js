40ec4d59c292828e89b5a490b0aeefee26a5518b
MOHDSUHEL_01 <mohdsuhel.dev@gmail.com>
2024-12-25 01:23:49 +0530

    first page

--- Assignment-5/server.js
+++ Assignment-5/server.js
@@ -8,13 +8,15 @@ const mongodbUrl = process.env.MONGODB_URL
 const port = process.env.PORT
 
 
-const userRouter =require('./Routers/userRouter')
-app.use('/',userRouter)
+const userRouter = require('./Routers/userRouter')
+app.use('/', userRouter)
 
-app.get('/',(req,res)=>{
-    res.sent('<h1>Wellcome to MERN assignment-5   -Mohd Suhel</h1>')
-})
-app.listen(port,()=>{
+app.get('/', (req, res) => {
+    res.send(
+        '<div align="center" style="color:black;"><h1 style="color:red;">Welcome to MERN Assignment5 - Mohd Suhel</h1><h3>Every thing is query base</h3> <p>For user register use- /register?fields &nbsp;  *email is required</p>For user update use- /update/user_id?fields <p><p>For user delete user- /delete/user_id</p></p></div>');
+});
+
+app.listen(port, () => {
     console.log(`Server is running on port : ${port}`)
 })
 mongoose.connect(mongodbUrl).then(() => console.log('Db connected')).catch((err) => console.log('db not connected', err))
